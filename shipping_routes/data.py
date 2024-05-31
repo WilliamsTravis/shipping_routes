aee7a5692c053b1402906482117875f6cbbfd312
williamstravis <twillico@proton.me>
2024-05-30 19:08:27 -0600

    Added initial data building script.

---
+++ shipping_routes/data.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  6 20:29:57 2024
+
+@author: travis
+"""
+import requests
+
+from bs4 import BeautifulSoup as bs
+
+
+URLS = {
+    "wb": ("https://datacatalog.worldbank.org/search/dataset/0037580/"
+           "Global-Shipping-Traffic-Density")
+}
+
+
+class WorldBank:
+    """Methods for Downloading from the world bank."""
+
+    def __init__(self, url=URLS["wb"]):
+        """Initialize WorldBank object."""
+        self.url = url
+
+    @property
+    def paths(self):
+        """Return paths to all datasets."""
+        with requests.get(self.url) as r:
+            content = r.content
+        soup = bs(content)
+        for a in soup.find_all('a', href=True):
+            print("Found the URL:", a['href'])
+
+if __name__ == "__main__":
+    self = WorldBank()
